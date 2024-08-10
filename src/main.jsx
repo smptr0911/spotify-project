09b27b3bf5282f10645796ff361b38c0a4327128
Bedro Park <smptr0911@naver.com>
2024-08-09 21:57:55 -0700

    reordered the rest imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import App from './App.jsx';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
